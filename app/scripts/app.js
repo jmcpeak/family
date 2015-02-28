221790803d7968d4511bd117b2a5ac4f259666ad
jmcpeak <jason.mcpeak@gmail.com>
2015-02-28 18:40:04 -0500

    Initial check-in - built with yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name familyApp
+ * @description
+ * # familyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('familyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
