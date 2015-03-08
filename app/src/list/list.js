fcf4a9c2b8284f99eb7844d1f529cb19103099c6
jmcpeak <jason.mcpeak@gmail.com>
2015-03-07 20:59:42 -0500

    some more thoughts

--- app/src/list/list.js
+++ app/src/list/list.js
@@ -31,24 +31,17 @@ angular.module('jmList', ['ngMaterial', 'jmUser'])
                     window.alert('Failed: ' + reason);
                 });
 
-                $scope.showUserBottom = function (event) {
-                    $mdBottomSheet.show({
-                        templateUrl: 'scripts/user-bottom-sheet.tpl.html',
-                        controller: 'jmFloatingButtonController',
-                        targetEvent: event
-                    });
-                };
-
                 $scope.showUser = function (event) {
                     $mdDialog.show({
                         controller: 'jmUserController',
                         templateUrl: 'src/user/user.tpl.html',
                         targetEvent: event
-                    }).then(function (answer) {
-                        $scope.alert = 'You said the information was "' + answer + '".';
-                    }, function () {
-                        $scope.alert = 'You cancelled the dialog.';
                     });
+                    //.then(function (answer) {
+                    //    $scope.alert = 'You said the information was "' + answer + '".';
+                    //}, function () {
+                    //    $scope.alert = 'You cancelled the dialog.';
+                    //});
                 };
             }
         };
