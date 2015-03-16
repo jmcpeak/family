cfa61c832bac8d68cc1d872da15ea53b76432c22
jmcpeak <jason.mcpeak@gmail.com>
2015-03-15 20:46:36 -0400

    more changes

--- src/app/list/list.js
+++ src/app/list/list.js
@@ -11,39 +11,20 @@ angular.module('jmList', ['ngMaterial', 'jmUser', 'jmViewEdit'])
     .directive("jmList", function () {
         return {
             templateUrl: 'list/list.tpl.html',
-            controller: function ($scope, $mdDialog, $mdSidenav, jmDB) {
+            controller: function ($scope, $mdSidenav, jmDB) {
                 var promise = jmDB.queryAll();
-
-                promise.then(function (users) {
-                    $scope.count = users.length;
-                    for (var i in users) {
-                        $scope.users.push({
-                            id: users[i].id,
-                            //face: '',
-                            who: users[i].firstName + ' ' + users[i].lastName,
-                            where: users[i].address,
-                            email: users[i].email,
-                            initials: users[i].firstName[0] + users[i].lastName[0]
-                        });
-                    }
-                }, function (reason) {
-                    window.alert('Failed: ' + reason);
-                });
-
                 $scope.users = [];
                 $scope.count = '';
                 $scope.lastUpdate = new Date();
                 $scope.name = 'McPeak';
-
                 $scope.height = 'auto';
 
-                $scope.showUser = function (event, user) {
-                    $mdDialog.show({
-                        templateUrl: 'user/dialog.tpl.html',
-                        targetEvent: event,
-                        locals: user
-                    });
-                };
+                promise.then(function (data) {
+                    $scope.count = data.length;
+                    $scope.users = data;
+                }, function (reason) {
+                    window.alert('Failed: ' + reason);
+                });
 
                 $scope.open = function () {
                     $mdSidenav('left').open();
@@ -56,18 +37,10 @@ angular.module('jmList', ['ngMaterial', 'jmUser', 'jmViewEdit'])
                 $scope.showDelete = false;
 
                 $scope.menus = [
-                    {link: 'https://github.com/jmcpeak/family/issues', label: 'issues'},
-                    {link: 'https://github.com/jmcpeak/family', label: 'Code'},
-                    {link: 'https://travis-ci.org/jmcpeak/family', label: 'Builds'}
+                    {link: 'https://github.com/jmcpeak/family/issues', label: 'Log an Issue'},
+                    {link: 'https://github.com/jmcpeak/family', label: 'See the code'},
+                    {link: 'https://travis-ci.org/jmcpeak/family', label: 'View Builds'}
                 ];
-
-                $scope.add = function (event) {
-                    $mdDialog.show({
-                        template: '<md-dialog aria-label="Add" style="width: 30%; height: 30%;"><md-content layout-padding flex>Add User</md-content></md-dialog>',
-                        targetEvent: event
-                    });
-                };
-
             }
         };
     });
--- src/app/user/user.js
+++ src/app/user/user.js
@@ -27,21 +27,31 @@ angular.module('jmUser', ['ngMaterial'])
         };
     })
 
-    .controller("jmUserController", function ($scope, $location, jmDB) {
-
-        var locals = {id: '1'};
-
+    .controller("jmUserController", function ($scope, $location, $mdDialog) {
         $scope.tabs = ['required', 'additional', 'spouse', 'dates and places', 'children / pets'];
         $scope.selectedIndex = 0;
+
+        $scope.update = function (user) {
+            $scope.selectedUser = user;
+        };
+
+        $scope.add = function (event) {
+            $scope.addUser = true;
+            $mdDialog.show({
+                templateUrl: 'user/dialog.tpl.html',
+                targetEvent: event
+            });
+        };
 
         $scope.next = function () {
             $scope.selectedIndex = Math.min($scope.selectedIndex + 1, 2);
         };
+
         $scope.previous = function () {
             $scope.selectedIndex = Math.max($scope.selectedIndex - 1, 0);
         };
 
-        $scope.switch = function (index) {
+        $scope.partial = function (index) {
             switch (index) {
                 case 0:
                     $location.path('/required');
@@ -63,51 +73,4 @@ angular.module('jmUser', ['ngMaterial'])
                     break;
             }
         };
-
-        jmDB.getUser(locals.id).then(function (data) {
-            $scope.user = data;
-        }, function (reason) {
-            window.alert('Failed: ' + reason);
-        });
-
-        {
-
-            //function loadAll() {
-            //    var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming';
-            //    return allStates.split(/, +/g).map(function (state) {
-            //        return {
-            //            value: state.toLowerCase(),
-            //            display: state
-            //        };
-            //    });
-            //}
-            //
-            //function querySearch(query) {
-            //    var results = query ? $scope.states.filter(createFilterFor(query)) : [],
-            //        deferred;
-            //    if ($scope.simulateQuery) {
-            //        deferred = $q.defer();
-            //        $timeout(function () {
-            //            deferred.resolve(results);
-            //        }, Math.random() * 1000, false);
-            //        return deferred.promise;
-            //    } else {
-            //        return results;
-            //    }
-            //}
-            //
-            //function createFilterFor(query) {
-            //    var lowercaseQuery = angular.lowercase(query);
-            //    return function filterFn(state) {
-            //        return (state.value.indexOf(lowercaseQuery) === 0);
-            //    };
-            //}
-
-            //$scope.states = loadAll();
-            //$scope.selectedItem = undefined;
-            //$scope.searchText = undefined;
-            //$scope.querySearch = querySearch;
-            //$scope.simulateQuery = false;
-            //$scope.isDisabled = false;
-        }
     });
