5571e6b8ac32a8cfe0c4bed0e6c4200909c39d29
mcpeakj <jason.mcpeak@gmail.com>
2015-06-25 00:49:13 -0400

    more cleanup

--- src/app/list/list.js
+++ src/app/list/list.js
@@ -6,7 +6,59 @@ angular.module('jmList', ['ngMaterial', 'jmUser', 'jmInput'])
         return {
             replace: true,
             templateUrl: 'list/list.tpl.html',
-            controller: 'jmListController'
+            controller: function ($scope, $rootScope, $timeout, $localStorage, jmDB, jmConstant) {
+                $scope.users = [];
+                $scope.count = '';
+                $scope.height = 'auto';
+                $scope.showDelete = false;
+                $scope.queryAllInProgress = false;
+
+                var init = function () {
+                    jmDB.getItem('lastUpdateDate').then(function (data) {
+                        $timeout(function () {
+                            $scope.lastUpdate = data.lastUpdated;
+                        });
+                    });
+
+                    $scope.refresh().then(function () {
+                        if ($localStorage.user) {
+                            $timeout(function () {
+                                angular.element(jmConstant.userIdHash + $localStorage.user.id).click()[0].scrollIntoView(false);
+                            }, 100);
+                        }
+                    });
+                };
+
+                $rootScope.$on('refresh', function (event, user) {
+                    $scope.refresh(user);
+                });
+
+                $scope.refresh = function (user) {
+                    if (!user) {
+                        $scope.queryAllInProgress = true;
+                    }
+
+                    return jmDB.queryAll().then(function (data) {
+                        $timeout(function () {
+
+                            $scope.count = data.length;
+                            $scope.users = data;
+                            if (user) {
+                                $scope.selectUser(user);
+                            } else {
+                                $scope.queryAllInProgress = false;
+                            }
+                        });
+                    }, function (reason) {
+                        $timeout(function () {
+                            $scope.queryAllInProgress = true;
+                            $scope.queryAllError = reason.message ? reason.message : 'Unknown Error';
+                        });
+                    });
+                };
+
+                init();
+            }
         };
     })
 
@@ -15,58 +67,4 @@ angular.module('jmList', ['ngMaterial', 'jmUser', 'jmInput'])
             replace: true,
             templateUrl: 'list/toolbar.tpl.html'
         };
-    })
-
-    .controller('jmListController', function ($scope, $rootScope, $timeout, $localStorage, jmDB, jmConstant) {
-        $scope.users = [];
-        $scope.count = '';
-        $scope.height = 'auto';
-        $scope.showDelete = false;
-        $scope.queryAllInProgress = false;
-
-        var init = function () {
-            jmDB.getItem('lastUpdateDate').then(function (data) {
-                $timeout(function () {
-                    $scope.lastUpdate = data.lastUpdated;
-                });
-            });
-
-            $scope.refresh().then(function() {
-                if ($localStorage.user) {
-                    $timeout(function () {
-                        angular.element(jmConstant.userIdHash + $localStorage.user.id).click()[0].scrollIntoView(false);
-                    }, 100);
-                }
-            });
-        };
-
-        $rootScope.$on('refresh', function (event, user) {
-            $scope.refresh(user);
-        });
-
-        $scope.refresh = function (user) {
-            if (!user) {
-                $scope.queryAllInProgress = true;
-            }
-
-            return jmDB.queryAll().then(function (data) {
-                $timeout(function () {
-
-                    $scope.count = data.length;
-                    $scope.users = data;
-                    if (user) {
-                        $scope.selectUser(user);
-                    } else {
-                        $scope.queryAllInProgress = false;
-                    }
-                });
-            }, function (reason) {
-                $timeout(function () {
-                    $scope.queryAllInProgress = true;
-                    $scope.queryAllError = reason.message ? reason.message : 'Unknown Error';
-                });
-            });
-        };
-
-        init();
     });
--- src/app/user/user.js
+++ src/app/user/user.js
@@ -14,140 +14,131 @@ angular.module('jmUser', ['ngMaterial', 'jmPartials'])
         };
     })
 
-    .controller('jmUserController', function ($scope, $rootScope, $mdDialog, $mdToast, $localStorage, jmDB, jmService, jmConstant) {
-        $scope.tabs = [
-            {name: 'basic', position: 0},
-            {name: 'additional', position: 1},
-            {name: 'spouse', position: 2},
-            {name: 'dates and places', position: 3},
-            {name: 'children / pets', position: 4}];
-        $scope.selectedTab = 0;
-
-        var toast = function (msg, error) {
-            var errorClass = (error) ? 'class="error"' : undefined;
-
-            $mdToast.show({
-                template: '<md-toast ' + errorClass + '><span><b>' + msg + '</b></span></md-toast>',
-                hideDelay: 2000,
-                position: 'bottom right'
-            });
-        };
-
-        $scope.deleteItem = function (event) {
-            var pre = 'Remove ';
-            var fn = $scope.selectedUser.firstName ? $scope.selectedUser.firstName : '';
-            var ln = $scope.selectedUser.lastName ? $scope.selectedUser.lastName : '';
-            var name = fn + ' ' + ln;
-            var title = name.length > 1 ? pre + name + ' - are you sure?' : pre + 'user?';
-
-            $mdDialog.show($mdDialog.confirm()
-                .title(title)
-                .content('All the data and the entry itself will be removed')
-                .ok('Remove')
-                .cancel('Cancel')
-                .targetEvent(event)).then(function () {
-                jmDB.deleteItem($scope.selectedUser).then(
-                    function () {
-                        var id = $scope.selectedUser.id;
-
-                        // Delete the card
-                        angular.element(jmConstant.userIdHash).remove();
-
-                        // Update count
-                        $scope.count--;
-
-                        // Remove from users array
-                        for (var i in $scope.users) {
-                            if ($scope.users[i].id === id) {
-                                $scope.users.splice(i, 1);
-                            }
-                        }
-
-                        if ($scope.users.length >= 1) {
-                            $scope.selectUser($scope.users[0]);
-                        }
-
-                        toast('User Removed');
-                    },
-                    function () {
-                        toast('There was a problem removing...', true);
-                    });
-            });
-        };
-
-        $scope.isRemoveDisabled = function () {
-            return !angular.isDefined($scope.selectedUser);
-        };
-
-        $scope.isSaveDisabled = function () {
-            return jmService.getRequiredForm() && jmService.getRequiredForm().$invalid;
-        };
-
-        $scope.selectUser = function (user) {
-            $scope.selectedUser = user;
-            $localStorage.user = user;
-            $scope.selectedTab = 0;
-
-            jmService.resetPreviousCard();
-            jmService.setSelectedCard(angular.element(jmConstant.userIdHash + user.id));
-        };
-
-        $scope.putItem = function () {
-            jmDB.putItem($scope.selectedUser).then(
-                function () {
-                    toast(($scope.addUser) ? 'User Added' : 'User Saved');
-
-                    if ($scope.addUser) {
-                        $mdDialog.hide();
-                        $rootScope.$emit('refresh', $scope.selectedUser);
-                    }
-                },
-                function () {
-                    toast('There was a problem saving...', true);
-                });
-        };
-
-        $scope.add = function (event) {
-
-            $scope.addUser = true;
-
-            var newScope = $scope.$new();
-            newScope.addUser = true;
-
-            $mdDialog.show({
-                controller: 'jmDialogController',
-                templateUrl: 'user/dialog.tpl.html',
-                targetEvent: event,
-                clickOutsideToClose: false,
-                scope: newScope,
-                focusOnOpen: false
-            }).then(function () {
-                $scope.addUser = false;
-            }, function () {
-                $scope.addUser = false;
-            });
-        };
-
-        $scope.next = function () {
-            $scope.selectedTab = Math.min($scope.selectedTab + 1, 2);
-        };
-
-        $scope.previous = function () {
-            $scope.selectedTab = Math.max($scope.selectedTab - 1, 0);
-        };
-    })
-
     .directive('jmUser', function () {
         return {
             templateUrl: 'user/user.tpl.html',
-            controller: 'jmUserController'
-        };
-    })
+            controller: function ($scope, $rootScope, $mdDialog, $mdToast, $localStorage, jmDB, jmService, jmConstant) {
+                $scope.tabs = [
+                    {name: 'basic', position: 0},
+                    {name: 'additional', position: 1},
+                    {name: 'spouse', position: 2},
+                    {name: 'dates and places', position: 3},
+                    {name: 'children / pets', position: 4}];
+                $scope.selectedTab = 0;
 
-    .directive('jmAddButton', function () {
-        return {
-            templateUrl: 'user/button.tpl.html',
-            controller: 'jmUserController'
+                var toast = function (msg, error) {
+                    var errorClass = (error) ? 'class="error"' : undefined;
+
+                    $mdToast.show({
+                        template: '<md-toast ' + errorClass + '><span><b>' + msg + '</b></span></md-toast>',
+                        hideDelay: 2000,
+                        position: 'bottom right'
+                    });
+                };
+
+                $scope.deleteItem = function (event) {
+                    var pre = 'Remove ';
+                    var fn = $scope.selectedUser.firstName ? $scope.selectedUser.firstName : '';
+                    var ln = $scope.selectedUser.lastName ? $scope.selectedUser.lastName : '';
+                    var name = fn + ' ' + ln;
+                    var title = name.length > 1 ? pre + name + ' - are you sure?' : pre + 'user?';
+
+                    $mdDialog.show($mdDialog.confirm()
+                        .title(title)
+                        .content('All the data and the entry itself will be removed')
+                        .ok('Remove')
+                        .cancel('Cancel')
+                        .targetEvent(event)).then(function () {
+                        jmDB.deleteItem($scope.selectedUser).then(
+                            function () {
+                                var id = $scope.selectedUser.id;
+
+                                // Delete the card
+                                angular.element(jmConstant.userIdHash).remove();
+
+                                // Update count
+                                $scope.count--;
+
+                                // Remove from users array
+                                for (var i in $scope.users) {
+                                    if ($scope.users[i].id === id) {
+                                        $scope.users.splice(i, 1);
+                                    }
+                                }
+
+                                if ($scope.users.length >= 1) {
+                                    $scope.selectUser($scope.users[0]);
+                                }
+
+                                toast('User Removed');
+                            },
+                            function () {
+                                toast('There was a problem removing...', true);
+                            });
+                    });
+                };
+
+                $scope.isRemoveDisabled = function () {
+                    return !angular.isDefined($scope.selectedUser);
+                };
+
+                $scope.isSaveDisabled = function () {
+                    return jmService.getRequiredForm() && jmService.getRequiredForm().$invalid;
+                };
+
+                $scope.selectUser = function (user) {
+                    $scope.selectedUser = user;
+                    $localStorage.user = user;
+                    $scope.selectedTab = 0;
+
+                    jmService.resetPreviousCard();
+                    jmService.setSelectedCard(angular.element(jmConstant.userIdHash + user.id));
+                };
+
+                $scope.putItem = function () {
+                    jmDB.putItem($scope.selectedUser).then(
+                        function () {
+                            toast(($scope.addUser) ? 'User Added' : 'User Saved');
+
+                            if ($scope.addUser) {
+                                $mdDialog.hide();
+                                $rootScope.$emit('refresh', $scope.selectedUser);
+                            }
+                        },
+                        function () {
+                            toast('There was a problem saving...', true);
+                        });
+                };
+
+                $scope.add = function (event) {
+
+                    $scope.addUser = true;
+
+                    var newScope = $scope.$new();
+                    newScope.addUser = true;
+
+                    $mdDialog.show({
+                        controller: 'jmDialogController',
+                        templateUrl: 'user/dialog.tpl.html',
+                        targetEvent: event,
+                        clickOutsideToClose: false,
+                        scope: newScope,
+                        focusOnOpen: false
+                    }).then(function () {
+                        $scope.addUser = false;
+                    }, function () {
+                        $scope.addUser = false;
+                    });
+                };
+
+                $scope.next = function () {
+                    $scope.selectedTab = Math.min($scope.selectedTab + 1, 2);
+                };
+
+                $scope.previous = function () {
+                    $scope.selectedTab = Math.max($scope.selectedTab - 1, 0);
+                };
+            }
         };
     })
 
